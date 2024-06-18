cefd14d1657685c6a78497179bc0a1cfb93ea48c
StanleySnyder <161008028+StanleySnyder@users.noreply.github.com>
2024-06-18 10:28:13 +0300

    Create StreamService.java

---
+++ StreamService.java
@@ -0,0 +1,9 @@
+import java.util.Collections;
+import java.util.List;
+
+public class StreamService {
+    public void sortStreams(List<Stream> streams) {
+        StreamComparator comparator = new StreamComparator();
+        Collections.sort(streams, comparator);
+    }
+}
