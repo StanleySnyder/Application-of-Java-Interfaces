a1713090a96eb9dff6ebb37de137c3f72b67d754
StanleySnyder <161008028+StanleySnyder@users.noreply.github.com>
2024-06-18 10:27:15 +0300

    Create Group.java

---
+++ Group.java
@@ -0,0 +1,18 @@
+public class Group {
+    private String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
